01766bc52911d37276dc74b9273da31771fe8e34
Marko <sheme4ko@mail.ru>
2015-01-31 22:39:44 +0200

    push with deploy

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-var_dump(44444444444);
+var_dump(4444444442244);
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
